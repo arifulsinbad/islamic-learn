d3bf7036f69a83352bd0bad7af1680ef6debd6df
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-10-26 16:43:32 +0600

    commit 3

---
+++ src/Layout/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import RightSide from '../RightSide/RightSide';
+
+const Main = () => {
+ return (
+  <div>
+   <Header></Header>
+   <Container>
+    <Row>
+     <Col lg='9'>
+      <Outlet></Outlet>
+     </Col>
+     <Col lg='3'>
+      <RightSide></RightSide>
+     </Col>
+    </Row>
+    
+   </Container>
+   <Footer></Footer>
+  </div>
+ );
+};
+
+export default Main;
