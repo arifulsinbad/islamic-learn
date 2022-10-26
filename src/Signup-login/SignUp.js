387a57c390e2e60c9c1b47300f32de785fb70628
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-10-26 17:42:48 +0600

    commit 6

--- src/Signup-login/SignUp.js
+++ src/Signup-login/SignUp.js
@@ -22,7 +22,7 @@ const SignUp = () => {
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control name='email' type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -30,7 +30,7 @@ const SignUp = () => {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control name='password' type="password" placeholder="Password" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
